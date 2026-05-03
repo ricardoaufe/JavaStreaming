fba73ec63780f58a64067b74b2365dd98e6fddbe
ricardoaufe <rasf0831@gmail.com>
2026-05-03 09:46:21 -0300

    Cria classe Main
    Classe Main para inicialização do sistema

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ricardo Ferreira
+ */
+import view.Login;
+
+public class Main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new Login().setVisible(true);
+        });
+    }
+}
